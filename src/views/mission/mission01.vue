a6b3490dbf1c67aadf86924dde9f36f52ba4faa9
soyoung-v <ssssy93@icloud.com>
2025-12-29 16:25:04 +0900

    mission

---
+++ src/views/mission/mission01.vue
@@ -0,0 +1,24 @@
+ <script setup>
+ const students = [
+  { name: '수영', present: true },
+  { name: '준하', present: false },
+  { name: '가은', present: true },
+  { name: '수빈', present: true },
+  { name: '예림', present: false },
+  { name: '준이', present: true }
+];
+ </script>
+ 
+ <template>
+    <h3>mission01</h3>
+    <div v-for="(item, idx) in students" :key="idx">
+        {{ item.name }}
+        <span v-if="item.present">출석</span>
+    </div>
+
+
+ </template>
+ 
+ <style scoped>
+ 
+ </style>
